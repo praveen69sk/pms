7b12184c355817549da4e329f0bcca6e9886d153
praveen69sk <praveen69sk@gmail.com>
2025-03-24 23:39:29 +0530

    Initial commit of billing_service

---
+++ billing-service/src/main/java/com/praveensk/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,8 @@
+package com.praveensk.billingservice.grpc;
+
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+}
