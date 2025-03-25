b0a6184c18efbc7b91549f20bfa9b370976c818e
praveen69sk <praveen69sk@gmail.com>
2025-03-25 23:10:11 +0530

    Implemented validate token functionality

--- auth-service/src/main/java/com/praveensk/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/praveensk/authservice/service/AuthService.java
@@ -2,6 +2,7 @@ package com.praveensk.authservice.service;
 
 import com.praveensk.authservice.dto.LoginRequestDTO;
 import com.praveensk.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,5 +27,14 @@ public class AuthService {
                 .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
                 .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
         return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
     }
 }
